119aaa110082a554f262689e1a124096a1546df9
Anurup Borah <anuborah@sph.com.sg>
2023-04-14 10:01:40 +0800

    Change in the condition logic

--- .github/workflows/check.sh
+++
@@ -1,7 +0,0 @@
-if [ "" != "" ]; then
-  echo 'file_output=trivy-results-docker.sarif'
-elif [ "newslink/config-dev" != "" ] &&  [ "564613846094" != "" ]; then
-  echo "file_output=trivy-results-ecr.sarif"
-else
-  echo "file_output="
-fi
